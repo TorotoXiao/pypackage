6a6c7ea0876d9b8cafef5b971c1ca28d06286759
sophiazhou <sophia.zhou@kucoin.com>
2020-06-10 20:35:30 +0800

    update code

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='testpackage',
-    version='1.0.4',
+    version='1.0.5',
     packages=['testpackage'],
     license="MIT",
     author='Sophia',
     author_email="Sophia@qq.com",
     url='https://github.com/TorotoXiao/pypackage',
     description="test publish package to github",
-    install_requires=['pycryptodome'],
+    install_requires=["pycryptodome"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
