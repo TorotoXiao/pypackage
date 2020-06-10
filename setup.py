1a2ba5fb0bf44d28323b4053449c1756c372e34e
sophiazhou <sophia.zhou@kucoin.com>
2020-06-10 15:29:46 +0800

    init

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='test_package',
+    version='1.0.0',
+    packages=[],
+    license="MIT",
+    author='Sophia',
+    author_email="Sophia@qq.com",
+    url='https://github.com/TorotoXiao/pypackage',
+    description="test publish package to github",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
